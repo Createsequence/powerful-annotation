88286f3414bba96e873f2ea4d31697754cc09c01
huangchengxing <huangchengxing@nova.net.cn>
2022-07-23 13:25:44 +0800

    feat: add annotation scanner for annotated element

---
+++ src/main/java/top/xiajibagao/powerfulannotation/scanner/GenericTypeHierarchyAnnotationScanner.java
@@ -0,0 +1,254 @@
+package top.xiajibagao.powerfulannotation.scanner;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.ObjectUtil;
+import lombok.Getter;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+/**
+ * <p>支持对类层次结构进行扫描的扫描器通用实现
+ *
+ * <p>当{@link AnnotatedElement}类型不同时，从层级结构中扫描注解的对象将有所区别：
+ * <ul>
+ *     <li>
+ *         当元素为{@link Class}时，此处层级结构即指类本身与其父类、父接口共同构成的层级结构，
+ *         扫描器将扫描层级结构中类、接口声明的注解；
+ *     </li>
+ *     <li>
+ *         当元素为{@link Method}时，此处层级结构指声明方法的类的层级结构，
+ *         扫描器将从层级结构中寻找与该方法签名相同的非桥接方法，并对其进行扫描；
+ *     </li>
+ *     <li>
+ *         当元素为{@link Field}时，此处层级结构指声明属性的类的层级结构，
+ *         扫描器将从层级结构中寻找与该属性具有相同类型与名称的属性，并对其注解进行扫描；
+ *     </li>
+ *     <li>当元素不为{@link Method}或{@link Class}时，则其层级结构仅有其本身一层；</li>
+ * </ul>
+ * 此外，扫描器支持在获取到层级结构中的注解对象后，再对注解对象的元注解进行扫描。
+ *
+ * @author huangchengxing
+ */
+@Getter
+public class GenericTypeHierarchyAnnotationScanner extends AbstractTypeHierarchyScanner {
+
+    /**
+     * 是否支持扫描父类
+     */
+    private final boolean enableScanSuperClass;
+
+    /**
+     * 是否支持扫描接口
+     */
+    private final boolean enableScanInterface;
+
+    /**
+     * 是否支持扫描父类
+     */
+    private final boolean enableScanMetaAnnotation;
+
+    /**
+     * 构造一个通用注解扫描器，默认不处理包括{@link java.lang}, 与{@link javax}还有{@link com.sun}包下的类
+     *
+     * @param enableScanSuperClass 是否支持扫描父类
+     * @param enableScanInterface 是否支持扫描接口
+     * @param enableScanMetaAnnotation 是否支持扫描父类
+     */
+    public GenericTypeHierarchyAnnotationScanner(
+        boolean enableScanSuperClass, boolean enableScanInterface, boolean enableScanMetaAnnotation) {
+        this(
+            enableScanSuperClass, enableScanInterface, enableScanMetaAnnotation,
+            t -> !CharSequenceUtil.startWithAny(t.getName(), "java.lang", "javax", "com.sum")
+        );
+    }
+
+    /**
+     * 构造一个通用注解扫描器
+     *
+     * @param enableScanSuperClass 是否支持扫描父类
+     * @param enableScanInterface 是否支持扫描接口
+     * @param enableScanMetaAnnotation 是否支持扫描父类
+     * @param typeFilter 类型过滤器，若该类型无法通过过滤器，则不会被扫描器扫描
+     */
+    public GenericTypeHierarchyAnnotationScanner(
+        boolean enableScanSuperClass,
+        boolean enableScanInterface,
+        boolean enableScanMetaAnnotation,
+        Predicate<Class<?>> typeFilter) {
+        super(typeFilter);
+        this.enableScanSuperClass = enableScanSuperClass;
+        this.enableScanInterface = enableScanInterface;
+        this.enableScanMetaAnnotation = enableScanMetaAnnotation;
+    }
+
+    // ======================== 扫描注解 ========================
+
+    /**
+     * 收集当前类接口、父类或者元注解类，将其加入队列用于下一次递归获取注解
+     *
+     * @param nextTypeHierarchies 队列
+     * @param type 目标类型
+     */
+    @Override
+    protected void collectToQueue(List<Class<?>> nextTypeHierarchies, Class<?> type) {
+        scanMetaAnnotationIfNecessary(nextTypeHierarchies, type);
+        scanSuperClassIfNecessary(nextTypeHierarchies, type);
+        scanInterfaceIfNecessary(nextTypeHierarchies, type);
+    }
+
+    /**
+     * 若{@link #enableScanMetaAnnotation}为{@code true}，则将目标类的父接口也添加到nextClasses
+     *
+     * @param nextTypeHierarchies 下一个类集合
+     * @param type 目标类型
+     */
+    protected void scanMetaAnnotationIfNecessary(List<Class<?>> nextTypeHierarchies, Class<?> type) {
+        if (!enableScanMetaAnnotation) {
+            return;
+        }
+        Stream.of(type.getDeclaredAnnotations())
+            .map(Annotation::annotationType)
+            .forEach(nextTypeHierarchies::add);
+    }
+
+    /**
+     * 若{@link #enableScanInterface}为{@code true}，则将目标类的父接口也添加到nextClasses
+     *
+     * @param nextTypeHierarchies 下一个类集合
+     * @param type 目标类型
+     */
+    protected void scanInterfaceIfNecessary(List<Class<?>> nextTypeHierarchies, Class<?> type) {
+        if (!enableScanInterface) {
+            return;
+        }
+        final Class<?>[] interfaces = type.getInterfaces();
+        if (ArrayUtil.isNotEmpty(interfaces)) {
+            CollUtil.addAll(nextTypeHierarchies, interfaces);
+        }
+    }
+
+    /**
+     * 若{@link #enableScanSuperClass}为{@code true}，则将目标类的父类也添加到nextClasses
+     *
+     * @param nextTypeHierarchies 下一个类队列
+     * @param type    目标类型
+     */
+    protected void scanSuperClassIfNecessary(List<Class<?>> nextTypeHierarchies, Class<?> type) {
+        if (!enableScanSuperClass) {
+            return;
+        }
+        final Class<?> superClass = type.getSuperclass();
+        if (!ObjectUtil.equals(superClass, Object.class) && ObjectUtil.isNotNull(superClass)) {
+            nextTypeHierarchies.add(superClass);
+        }
+    }
+
+    // ======================== 获取注解 ========================
+
+    /**
+     * <ul>
+     *     <li>若元素是{@link Class}，则直接返回；</li>
+     *     <li>若元素是{@link Member}，则返回{@link Member#getDeclaringClass()}；</li>
+     *     <li>若元素是不为上述两者，则返回{@code null};</li>
+     * </ul>
+     *
+     * @param element 待扫描的元素
+     * @return 元素的类
+     */
+    @Override
+    protected Class<?> getClassFormElement(AnnotatedElement element) {
+        if (element instanceof Class) {
+            return (Class<?>)element;
+        }
+        if (element instanceof Member) {
+            return ((Member)element).getDeclaringClass();
+        }
+        return null;
+    }
+    
+    /**
+     * <ul>
+     *     <li>若根元素是{@link Class}，则直接返回{@code type}上直接声明的注解；</li>
+     *     <li>若根元素是{@link Method}，则返回{@code type}中，与根元素具有完全一致的签名的非桥接方法上直接声明的注解；</li>
+     *     <li>若元素是{@link Field}，则返回{@code type}中，与跟原生具有一致的名称与类型的属性上直接声明的注解;</li>
+     *     <li>若元素不为上述三者中的任意一种，则将直接抛出{@link IllegalArgumentException}异常；</li>
+     * </ul>
+     *
+     * @param context 所有
+     * @param type 类型
+     * @return 待处理的注解
+     */
+    @Override
+    protected Annotation[] getAnnotationsFromType(Context context, Class<?> type) {
+        AnnotatedElement element = context.getSource();
+        // 扫描的元素是类
+        if (element instanceof Class) {
+            return type.getDeclaredAnnotations();
+        }
+        // 扫描的元素是方法
+        if (element instanceof Method) {
+            Method sourceMethod = (Method)element;
+            return Stream.of(ClassUtil.getDeclaredMethods(type))
+                .filter(superMethod -> !superMethod.isBridge())
+                .filter(superMethod -> hasSameMethodSignature(sourceMethod, superMethod))
+                .map(Method::getDeclaredAnnotations)
+                .flatMap(Stream::of)
+                .toArray(Annotation[]::new);
+        }
+        // 扫描的元素是属性
+        if (element instanceof Field) {
+            Field sourceField = (Field)element;
+            return Stream.of(ClassUtil.getDeclaredFields(type))
+                .filter(supperField -> hasSameFieldSignature(sourceField, supperField))
+                .map(Field::getDeclaredAnnotations)
+                .flatMap(Stream::of)
+                .toArray(Annotation[]::new);
+        }
+        throw new IllegalArgumentException(CharSequenceUtil.format(
+            "cannot get annotations from type [{}], because scanning source element type is [{}]", type, element
+        ));
+    }
+
+    /**
+     * 该方法是否具备与扫描的方法相同的方法签名
+     */
+    private boolean hasSameMethodSignature(Method sourceMethod, Method superMethod) {
+        if (ObjectUtil.equals(sourceMethod, sourceMethod)) {
+            return true;
+        }
+        if (!CharSequenceUtil.equals(sourceMethod.getName(), superMethod.getName())) {
+            return false;
+        }
+        final Class<?>[] sourceParameterTypes = sourceMethod.getParameterTypes();
+        final Class<?>[] targetParameterTypes = superMethod.getParameterTypes();
+        if (sourceParameterTypes.length != targetParameterTypes.length) {
+            return false;
+        }
+        if (!ArrayUtil.containsAll(sourceParameterTypes, targetParameterTypes)) {
+            return false;
+        }
+        return ClassUtil.isAssignable(superMethod.getReturnType(), sourceMethod.getReturnType());
+    }
+
+    /**
+     * 该方法是否具备与扫描的属性具有相同的类型和名称
+     */
+    private boolean hasSameFieldSignature(Field sourceField, Field superField) {
+        if (ObjectUtil.equals(sourceField, superField)) {
+            return true;
+        }
+        return CharSequenceUtil.equals(sourceField.getName(), superField.getName())
+            && ObjectUtil.equals(sourceField.getType(), superField.getType());
+    }
+
+}
---
+++ src/test/java/top/xiajibagao/powerfulannotation/scanner/processor/AnnotationFinderTest.java
@@ -0,0 +1,61 @@
+package top.xiajibagao.powerfulannotation.scanner.processor;
+
+import cn.hutool.core.util.ObjectUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * test for {@link AnnotationFinder}
+ *
+ * @author huangchengxing
+ */
+public class AnnotationFinderTest {
+
+    @Test
+    public void testAccept() {
+        AnnotationFinder finder = new AnnotationFinder(annotation -> ObjectUtil.equals(annotation.annotationType(), AnnotationForTest2.class));
+
+        AnnotationForTest1 annotationForTest1 = ClassForTest.class.getAnnotation(AnnotationForTest1.class);
+        finder.accept(0, 0, annotationForTest1);
+        Assert.assertFalse(finder.isFound());
+        Assert.assertFalse(finder.interrupted());
+        Assert.assertNull(finder.getTarget());
+
+        AnnotationForTest2 annotationForTest2 = ClassForTest.class.getAnnotation(AnnotationForTest2.class);
+        finder.accept(0, 0, annotationForTest2);
+        Assert.assertTrue(finder.isFound());
+        Assert.assertTrue(finder.interrupted());
+        Assert.assertEquals(annotationForTest2, finder.getTarget());
+
+        AnnotationForTest3 annotationForTest3 = ClassForTest.class.getAnnotation(AnnotationForTest3.class);
+        finder.accept(0, 0, annotationForTest3);
+        Assert.assertTrue(finder.isFound());
+        Assert.assertTrue(finder.interrupted());
+        Assert.assertEquals(annotationForTest2, finder.getTarget());
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
+    private @interface AnnotationForTest1 {}
+
+    @AnnotationForTest1
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
+    private @interface AnnotationForTest2 {}
+
+    @AnnotationForTest2
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.ANNOTATION_TYPE, ElementType.TYPE, ElementType.METHOD, ElementType.FIELD })
+    private @interface AnnotationForTest3 {}
+
+    @AnnotationForTest1
+    @AnnotationForTest2
+    @AnnotationForTest3
+    private static class ClassForTest{}
+
+}
